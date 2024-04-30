ad9da3466156a5494f60735d3c5afeb222c365b2
oliveeira2610 <bielolirodrigues@gmail.com>
2024-04-30 17:07:10 -0300

    commit

--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require("../controllers/usuario")
+const turmasController = require("../controllers/turmas")
 
 router.get('/usuario', usuarioController.getAll);
 router.get('/usuario/:id', usuarioController.getById);
 
 router.post('/usuario', usuarioController.createUsuario);
 
-module.exports = router;
 
+
+router.get('/turmas', turmasController.getAll);
+router.get('/turmas/:id', turmasController.getById);
+
+router.post('/turmas', turmasController.createTurma);
+
+module.exports = router;
