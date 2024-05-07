91b481a264782d37730c4f2711a8ea2583e99320
isamanzano <isabelacmanzano05@gamil.com>
2024-05-07 16:19:46 -0300

    h

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -20,3 +20,31 @@ exports.createUsuario = async (req, res) => {
     console.log("usuarioCriado", usuarioCriado);
     return res.send("Deu certo")
 };
+
+exports.updateUsuario = async (req,res) => {
+    const codigoUsuario = req.params.updateUsuario;
+    try{
+        const Usuariocadastrado = await Usuario.findOne({where: {codigo: cpfUsuario}});
+        if(Usuariocadastrado){
+            delete req.body.cpf; //medida de segurança
+
+            const [numRowsUpdated] = await Usuarios.update(req.body, { //define um array que faz contagem de numero de linhas que vao ser atualizadas, passadas na aquisiçao do body
+                where: {cpf: cpfUsuario}
+            })
+
+            if(numRowsUpdated > 0){
+                const usuarioAtualizado = await Usuarios.findOne({where:{cpf:cpfUsuario}});
+                return res.send({message: 'Usuario Atualizado com sucesso', usuariocomdadosnovos: usuarioAtualizado});
+            } else {
+                return res.send("Usuario encontrado, mas sem novos dados para atualizar");
+            }
+        } else{
+        return res.status(404).send("Não existe um usuario cadastrado com este codigo");
+        }
+
+
+    }catch (error) {
+        console.error("Erro ao atualizar usuario", error);
+        return res.status(500).send("Decorreu um erro ao atualizar o usuario");
+    }
+};
